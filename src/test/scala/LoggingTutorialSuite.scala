92f20cd8dfc148eae6729a1328931e0faf56e851
bsjoberg <brian.sjoberg@excella.com>
2019-11-02 12:26:08 -0400

    Loading from a properties file

---
+++ src/test/scala/LoggingTutorialSuite.scala
@@ -0,0 +1,32 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class LoggingTutorialSuite extends FunSuite {
+
+  test("Properties loading") {
+    val hello_world =
+      try {
+        val properties = new Properties()
+        properties.load(new FileInputStream("target/test-classes/config.properties"))
+        properties.getProperty("hello.world")
+      } catch {
+        case e: Exception =>
+          e.printStackTrace()
+          sys exit 1
+      }
+    assert("Hello World" == hello_world)
+  }
+
+  test("Logging tutorial file was created") {
+
+  }
+
+  test("Logging tutorial log info error debug messages") {
+    assert(true)
+  }
+}
